aee6ccb17512466de807be11d065cf2c9a17c27b
Olufemi-Olarewaju <olarewajufemi99@gmail.com>
2024-01-12 16:48:28 -0500

    All questions completed

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -71,6 +71,11 @@ where d.dept_name = 'Sales' or d.dept_name = 'Development'
 
 -- List the frequency counts, in descending order, of all the employee last names 
 -- (that is, how many employees share each last name)
+select last_name, count(last_name) as "Frequency Count"
+from employees
+group by last_name
+order by "Frequency Count" desc
+
 
 
 
